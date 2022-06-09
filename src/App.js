5a82876d7b0f470170f296aa80b3291f1038f645
brilliantdev <luckydeveloper1113@gmail.com>
2022-06-09 03:18:30 -0400

    soccer blurb

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Instructions from "./components/Instructions";
 import Promotion from "./components/Promotion";
 import AppPreview from "./components/AppPreview";
 import Presenters from "./components/Presenters";
+import Footballer from "./images/Footballer.png";
 
 function App() {
   return (
@@ -27,6 +28,24 @@ function App() {
       </div>
 
       <CodeSandbox appLink="https://codesandbox.io/embed/github/khuaulme/AtlasSearchWorkshop2022/tree/main/?fontsize=14&hidenavigation=1&theme=dark" />
+      <div className="flex px-20 items-center">
+        <div className="flex text-lg px-20 text-center ">
+          In May 2022, EA Sports and FIFA announced the end of their 30 year
+          partnership. As a tribute to this magnificent era, together we will
+          use Atlas Search to scour over the FIFA 22 dataset for the best
+          players with the most impossible names to build our final FIFA dream
+          team. Autocomplete, wildcard, and filters to find Ibrahimović and
+          Szczęsny? No problem!
+        </div>
+        <div>
+          {" "}
+          <img
+            src={Footballer}
+            alt="player"
+            className="w-full h-full object-cover "
+          />
+        </div>
+      </div>
       <CodeSandbox appLink="https://codesandbox.io/embed/friendly-rubin-chs0oe?fontsize=14&hidenavigation=1&theme=dark" />
       <Presenters />
 
