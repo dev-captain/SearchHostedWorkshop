66a55a2b4df29ae99662aaf6c464b229a319d161
brilliantdev <luckydeveloper1113@gmail.com>
2022-06-09 07:34:17 -0400

    added repo to website

--- src/App.js
+++ src/App.js
@@ -29,13 +29,25 @@ function App() {
 
       <CodeSandbox appLink="https://codesandbox.io/embed/github/khuaulme/AtlasSearchWorkshop2022/tree/main/?fontsize=14&hidenavigation=1&theme=dark" />
       <div className="flex px-20 items-center">
-        <div className="flex text-lg px-20 text-center ">
-          In May 2022, EA Sports and FIFA announced the end of their 30 year
-          partnership. As a tribute to this magnificent era, together we will
-          use Atlas Search to scour over the FIFA 22 dataset for the best
-          players with the most impossible names to build our final FIFA dream
-          team. Autocomplete, wildcard, and filters to find Ibrahimović and
-          Szczęsny? No problem!
+        <div>
+          <div className="flex text-xl px-20 text-center ">
+            In May 2022, EA Sports and FIFA announced the end of their 30 year
+            partnership. As a tribute to this magnificent era, together we will
+            use Atlas Search to scour over the FIFA 22 dataset for the best
+            players with the most impossible names to build our final FIFA dream
+            team. Autocomplete, wildcard, and filters to find Ibrahimović and
+            Szczęsny? No problem!
+          </div>
+          <div className=" flex my-6 w-1/5 p-4 border border-indigo-500 text-3xl items-center mx-auto text-center">
+            <a
+              className=" flex underline decoration-mongo-500 text-3xl mx-auto text-center"
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/mongodb-developer/atlas-search-soccer"
+            >
+              FIFA APPLICATION REPO
+            </a>
+          </div>
         </div>
         <div>
           {" "}
@@ -46,7 +58,8 @@ function App() {
           />
         </div>
       </div>
-      <CodeSandbox appLink="https://codesandbox.io/embed/friendly-rubin-chs0oe?fontsize=14&hidenavigation=1&theme=dark" />
+
+      <CodeSandbox appLink="https://codesandbox.io/embed/cool-microservice-iz4ugd?fontsize=14&hidenavigation=1&theme=dark" />
       <Presenters />
 
       <Footer />
