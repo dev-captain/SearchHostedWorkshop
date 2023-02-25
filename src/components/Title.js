3eceff0b9f31a281dff72135f4aa3e905d0a1686
brilliantseniordev <luckydeveloper1113@gmail.com>
2023-02-25 09:10:53 +0800

    update code

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="flex items-center mx-auto">
       <img src={SearchIcon} alt="search hero" className="mx-2 w-24" />
       <div className="font-noto text-center text-3xl">
-        Atlas Search: To Find it Fast, Look No Further
+        Fast Search: To Find it Fast, Look No Further
       </div>
     </div>
   );
